1627ead0b313230483eb1fb5b846a80620d83bf3
Jonathan Ho <ho.jonathan97@gmail.com>
2022-06-14 12:08:59 -0500

    init

---
+++ june_13/personal_work/lab.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("https://butterfly-conservation.org/uk-butterflies/a-to-z", verify=False)
+
+soup = BeautifulSoup(r.text, features="html.parser")
+
+links = soup.find_all("a")
+
+for link in links:
+    print(link.attrs.get('href'))
+
+hrefs = [link.attrs.get('href') for link in links]
+
+butterfly_pages = hrefs[39:100]
+
+urls = ["https://butterfly-conservation.org/" + page for page in butterfly_pages]
+
+# print (urls) 
+# We now have a list of urls attached to each butterfly
+
+def get_butterfly(url):
+    """Request and parse a single butterfly profile page, return a dict of data."""
+
+
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text)
+
+h1 = soup.find("h1")
+# print(h1)
+
+name = h1.text
+# strip() will take off whitespace at the end
+name = name.strip() 
+
